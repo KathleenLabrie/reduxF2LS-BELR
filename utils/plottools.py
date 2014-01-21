22bdf0ce7a79a1c17430ec9b47864102cfdcd644
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2014-01-21 08:01:42 -1000

    fixed bug in splot.  more pylint cleanup.

--- utils/plottools.py
+++ utils/plottools.py
@@ -1,67 +1,145 @@
+# plottools.py
+"""
+Collection of classes to help create plots.
+"""
 import matplotlib.pyplot as plt
 
 class Plot:
+    """
+    Base class for a plot.
+    
+    :param title: Plot title. Optional.
+    :type title: str
+    """
     def __init__(self, title=None):
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1,1,1)
-        for item in [self.fig, self.ax]:
+        self.axplot = self.fig.add_subplot(1, 1, 1)
+        for item in [self.fig, self.axplot]:
             item.patch.set_visible(False)
         if title is not None:
             self.set_title(title)
     
     def set_title(self, title):
-        self.ax.set_title(title)
+        """
+        Add a title to the plot.
+        
+        :param title: Title to assign to the plot.
+        :type title: str
+        """
+        self.axplot.set_title(title)
+
+    def set_axis_label(self, label, axis):
+        """
+        Add an axis label to the plot.
+        
+        :param label: Label to assign to axis.
+        :type title: str
+        :param axis: Name of the axis, x or y
+        :type axis: str
+        """
+        if axis == 'x':
+            self.axplot.set_xlabel(label)
+        elif axis == 'y':
+            self.axplot.set_ylabel(label)
+        else:
+            errmsg = 'Valid axis names are x and y.'
+            raise ValueError, errmsg
+
 
 class SpPlot(Plot):
+    """
+    Class to create and customize a spectrum plot. Subclasses Plot.
+    
+    :param title: Title to assign to the plot.
+    :type title: str
+    """
     def __init__(self, title=None):
         Plot.__init__(self, title)
     
-    def plot_spectrum(self, sp, title=None):
+    def plot_spectrum(self, sp1d, title=None):
+        """
+        Plot the spectrum (counts vs wavelength) with title and axis labels.
+        
+        :param sp1d: A Spectrum instance.
+        :type sp1d: Spectrum object.
+        :param title: Title for the plot. Optional.
+        :type title: str
+        """
         if title is not None:
             self.set_title(title)
-        self.ax.set_xlabel(''.join(['Wavelength [', sp.wunit.name,']']))
-        self.ax.set_ylabel('Counts')
-        self.ax.plot(sp.wlen, sp.counts, 'k')
-        self.ax.axis('tight')
+        self.set_axis_label(''.join(['Wavelength [', sp1d.wunit.name,']']), 'x')
+        self.set_axis_label('Counts', 'y')
+        self.axplot.plot(sp1d.wlen, sp1d.counts, 'k')
+        self.axplot.axis('tight')
         self.fig.canvas.draw()
         return
 
-    def adjust_ylimits(self, y1, y2):
-        self.ax.set_ylim(y1,y2)
+    def adjust_ylimits(self, ylim1, ylim2):
+        """
+        Adjust the lower and upper bounds to the y-axis.
+        
+        :param ylim1: Lower limit for the y-axis.
+        :type ylim1: float
+        :param ylim2: Upper limit for the y-axis.
+        :type ylim2: float
+        """
+        self.axplot.set_ylim(ylim1, ylim2)
         self.fig.canvas.draw()
         return
 
     def erase_plot(self, line_position=0):
-        self.ax.lines.pop(line_position).remove
+        """
+        Erase the spectrum but not the box and axes.
+        
+        :param line_position: Position on the stack of the spectrum to
+            erase.
+        :type line_position: int
+        """
+        self.axplot.lines.pop(line_position).remove
         self.fig.canvas.draw()
         return
 
     def annotate_lines(self, lines):
+        """
+        Annotate the plot with spectra line identifications.
+        
+        :param lines: The line list to add to the plot. The lines are stored
+            in a list of tuples with (obswlen, name), where obswlen is a float
+            and name is a string.
+        :type lines: list of tuples
+        """
         # lines is list of tuple (obswlen, name)
-        (xlow, xhigh) = self.ax.get_xlim()
-        (ylow, yhigh) = self.ax.get_ylim()
+        (xlow, xhigh) = self.axplot.get_xlim()
+        (_, yhigh) = self.axplot.get_ylim()
         ypos = yhigh * 0.8
         ydelta = yhigh * 0.05
-        i=0
+        i = 0
         for line in lines:
             if line[0] > xlow and line[0] < xhigh:
-                self.ax.text(line[0], ypos-(i*ydelta)-ydelta*1.25, '|',
+                self.axplot.text(line[0], ypos-(i*ydelta)-ydelta*1.25, '|',
                              horizontalalignment='center',
                              verticalalignment='center',
                              fontsize=10)
-                self.ax.text(line[0], ypos-(i*ydelta), line[1],
+                self.axplot.text(line[0], ypos-(i*ydelta), line[1],
                              horizontalalignment='center',
                              verticalalignment='center',
                              fontsize=10)
-                i+=1
+                i += 1
         self.fig.canvas.draw()
 
         return
 
-    def draw_band_limits(self):
-        return
+    #def draw_band_limits(self):
+    #    return
 
-    def write_png(self, output_name):        
+    def write_png(self, output_name):
+        """
+        Write the figure to a PNG file.
+        
+        :param output_name: Name of the output file. 
+            ??Is the extension .png required or is it added automatically??
+        :type output_name: str
+        """        
         self.fig.savefig(output_name)
         return
     
--- utils/specplot.py
+++ utils/specplot.py
@@ -1,5 +1,10 @@
-# plot spectrum
-# mask bands where atmosphere or response goes to zero  (see matplotlib note in Evernote)
+# specplot.py
+"""
+Utility function to plot a spectrum and annotate.
+"""
+
+# mask bands where atmosphere or response goes to zero  
+# (see matplotlib note in Evernote)
 # ID lines
 
 import spectro
@@ -10,31 +15,40 @@ def specplot(hdulist, spec_ext, title=None, line_list_name=None,
              output_plot_name=None, ylimits=None):
     # hdulist can be either astropy fits or pyfits
     
-    sp = spectro.Spectrum(hdulist[spec_ext])
+    spectrum = spectro.Spectrum(hdulist[get_valid_extension(spec_ext)])
     if line_list_name is not None:
         linelist = spectro.LineList(line_list_name, redshift)
     if draw_band_limits == True:
-        bandlist = spectro.BandList(sp.wlen[0],sp.wlen[-1])
+        bandlist = spectro.BandList(spectrum.wlen[0], spectrum.wlen[-1])
     
-    p = plottools.SpPlot(title=title)
-    p.plot_spectrum(sp)
+    plot = plottools.SpPlot(title=title)
+    plot.plot_spectrum(spectrum)
     if ylimits is not None:
-        p.adjust_ylimits(ylimits[0], ylimits[1])
+        plot.adjust_ylimits(ylimits[0], ylimits[1])
     
     if line_list_name is not None:
         lines_to_plot = []
         for line in linelist.lines:
-            lines_to_plot.append( (line.obswlen.to(sp.wunit).value, line.name) )
-        p.annotate_lines(lines_to_plot)
+            lines_to_plot.append( (line.obswlen.to(spectrum.wunit).value, 
+                                   line.name) )
+        plot.annotate_lines(lines_to_plot)
         
-    if draw_band_limits == True:
-        p.draw_band_limits()
+#    if draw_band_limits == True:
+#        plot.draw_band_limits()
     
     if output_plot_name is not None:
-        p.write_png(output_plot_name)
+        plot.write_png(output_plot_name)
     
     return
 
+def get_valid_extension(extension_string):
+    ext = extension_string.split(',')
+    if ext[0].isdigit():
+        valid_extension = int(ext[0])
+    else:
+        valid_extension = (ext[0].upper(), int(ext[1]))
+    
+    return valid_extension
 
 def example():
     import numpy as np
@@ -43,16 +57,16 @@ def example():
     from astrodata import AstroData
     
     ad = AstroData('JHK.fits')
-    x = np.arange(ad.get_key_value('NAXIS1'))
+    x_values = np.arange(ad.get_key_value('NAXIS1'))
     
-    w = wcs.WCS(ad.header.tostring())
-    wlen = w.wcs_pix2world(zip(x), 0)
+    wcs_ad = wcs.WCS(ad.header.tostring())
+    wlen = wcs_ad.wcs_pix2world(zip(x_values), 0)
     
-    plt.plot(wlen,ad.data)
+    plt.plot(wlen, ad.data)
     plt.xlabel('Wavelength [Angstrom]')
     plt.ylabel('Counts')
     plt.axis('tight')
-    plt.ylim(-100,800)
+    plt.ylim(-100, 800)
     plt.show()
     
     ad.close()
